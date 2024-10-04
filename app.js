4e22a4a26ab2f2c0f16a518a74ea6851dc1b76ab
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2024-10-04 14:51:31 -0300

    k

--- app.js
+++ app.js
@@ -62,16 +62,3 @@ function execCopy() {
   document.execCommand("copy");
 }
 
-// Type 1
-document.getElementById('execCopy').addEventListener('click', execCopy);
-function execCopy() {
-  document.querySelector("#input").select();
-  document.execCommand("copy");
-}
-
-// Type 2
-document.getElementById('clipboardCopy').addEventListener('click', clipboardCopy);
-async function clipboardCopy() {
-  let text = document.querySelector("#input").value;
-  await navigator.clipboard.writeText(text);
-}
